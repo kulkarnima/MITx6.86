8520258a461abb17681f6b67af32740a0fec5a03
Makarand Kulkarni <kulkarni.makarand@gmail.com>
2023-10-27 07:59:41 +0530

    Initial changes for Project 2

---
+++ Project 2/resources_mnist/mnist/part2-nn/test.py
@@ -0,0 +1,131 @@
+import os
+import sys
+import time
+import traceback
+import numpy as np
+import neural_nets
+
+sys.path.append("..")
+import utils
+
+verbose = False
+
+epsilon = 1e-6
+
+def green(s):
+    return '\033[1;32m%s\033[m' % s
+
+def yellow(s):
+    return '\033[1;33m%s\033[m' % s
+
+def red(s):
+    return '\033[1;31m%s\033[m' % s
+
+def log(*m):
+    print(" ".join(map(str, m)))
+
+def log_exit(*m):
+    log(red("ERROR:"), *m)
+    exit(1)
+
+
+def check_real(ex_name, f, exp_res, *args):
+    try:
+        res = f(*args)
+    except NotImplementedError:
+        log(red("FAIL"), ex_name, ": not implemented")
+        return True
+    if not np.isreal(res):
+        log(red("FAIL"), ex_name, ": does not return a real number, type: ", type(res))
+        return True
+    if not -epsilon < res - exp_res < epsilon:
+        log(red("FAIL"), ex_name, ": incorrect answer. Expected", exp_res, ", got: ", res)
+        return True
+
+
+def equals(x, y):
+    if type(y) == np.ndarray:
+        return (np.abs(x - y) < epsilon).all()
+    return -epsilon < x - y < epsilon
+
+def check_tuple(ex_name, f, exp_res, *args, **kwargs):
+    try:
+        res = f(*args, **kwargs)
+    except NotImplementedError:
+        log(red("FAIL"), ex_name, ": not implemented")
+        return True
+    if not type(res) == tuple:
+        log(red("FAIL"), ex_name, ": does not return a tuple, type: ", type(res))
+        return True
+    if not len(res) == len(exp_res):
+        log(red("FAIL"), ex_name, ": expected a tuple of size ", len(exp_res), " but got tuple of size", len(res))
+        return True
+    if not all(equals(x, y) for x, y in zip(res, exp_res)):
+        log(red("FAIL"), ex_name, ": incorrect answer. Expected", exp_res, ", got: ", res)
+        return True
+
+def check_array(ex_name, f, exp_res, *args):
+    try:
+        res = f(*args)
+    except NotImplementedError:
+        log(red("FAIL"), ex_name, ": not implemented")
+        return True
+    if not type(res) == np.ndarray:
+        log(red("FAIL"), ex_name, ": does not return a numpy array, type: ", type(res))
+        return True
+    if not len(res) == len(exp_res):
+        log(red("FAIL"), ex_name, ": expected an array of shape ", exp_res.shape, " but got array of shape", res.shape)
+        return True
+    if not equals(res, exp_res):
+        log(red("FAIL"), ex_name, ": incorrect answer. Expected", exp_res, ", got: ", res)
+
+        return True
+
+def check_list(ex_name, f, exp_res, *args):
+    try:
+        res = f(*args)
+    except NotImplementedError:
+        log(red("FAIL"), ex_name, ": not implemented")
+        return True
+    if not type(res) == list:
+        log(red("FAIL"), ex_name, ": does not return a list, type: ", type(res))
+        return True
+    if not len(res) == len(exp_res):
+        log(red("FAIL"), ex_name, ": expected a list of size ", len(exp_res), " but got list of size", len(res))
+        return True
+    if not all(equals(x, y) for x, y in zip(res, exp_res)):
+        log(red("FAIL"), ex_name, ": incorrect answer. Expected", exp_res, ", got: ", res)
+        return True
+
+def check_rectified_linear_unit():
+    ex_name = "ReLU unit"
+
+    x = [-1, 0, 1]
+    y = [0, 0, 1]
+
+    try:
+        r = neural_nets.rectified_linear_unit(0)
+    except NotImplementedError:
+        log(red("FAIL"), ex_name, ": not implemented")
+        return True
+    for i in range(len(x)):
+        exp = y[i]
+        got = neural_nets.rectified_linear_unit(x[i])
+        if (not equals(exp, got)):
+            log(
+                red("FAIL"), ex_name,
+                ": value for {} do not match. Expected {}, got {}"
+                .format(x[i], exp, got)
+            )
+    log(green("PASS"), ex_name, "")
+
+
+def main():
+    log(green("PASS"), "Import mnist project")
+    try:
+        check_rectified_linear_unit()
+    except Exception:
+        log_exit(traceback.format_exc())
+
+if __name__ == "__main__":
+    main()
